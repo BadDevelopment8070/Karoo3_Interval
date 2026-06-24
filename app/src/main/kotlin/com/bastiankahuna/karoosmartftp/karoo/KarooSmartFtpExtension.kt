89be07493d8abd7b8f71480926fbab14064cab0c
BadDevelopment8070 <baddevelopment8070@users.noreply.github.com>
2026-06-24 08:17:30 +0200

    v1.1.4: Update extension version and force UI refresh

--- app/src/main/kotlin/com/bastiankahuna/karoosmartftp/karoo/KarooSmartFtpExtension.kt
+++ app/src/main/kotlin/com/bastiankahuna/karoosmartftp/karoo/KarooSmartFtpExtension.kt
@@ -16,7 +16,7 @@ class KarooSmartFtpExtension : KarooExtension(EXTENSION_ID, EXTENSION_VERSION) {
 
     companion object {
         const val EXTENSION_ID = "smart_ftp"
-        const val EXTENSION_VERSION = "1.1.0"
+        const val EXTENSION_VERSION = "1.1.4"
         const val FIELD_TYPE_ID = "smart_ftp_field"
         const val ACTION_RESET = "reset_workout"
         const val ACTION_NEXT = "next_segment"
